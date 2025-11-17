8f0766a2a63dc6627685427b45afcd5311bffca4
Melanie Campuzano <Undepth71@gmail.com>
2025-11-17 14:10:12 -0800

    Feat: get pages to show and client's responses

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ def startOver():
     session.clear() #clears variable values and creates a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
-@app.route('/page1')
+@app.route('/page1',methods=['GET','POST'])
 def renderPage1():
     return render_template('page1.html')
 
@@ -34,28 +34,44 @@ def renderPage2():
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["God-like-Entity"]=request.form['God-like-Entity']
+    session["Ultimate-Life-Form"]=request.form['Ultimate-Life-Form']
     return render_template('page3.html')
 
 @app.route('/page4',methods=['GET','POST'])
-def renderPage3():
+def renderPage4():
     session["Location"]=request.form['Location']
     return render_template('page4.html')
 
 @app.route('/page5',methods=['GET','POST'])
-def renderPage3():
+def renderPage5():
     session["standName"]=request.form['standName']
     return render_template('page5.html')
 
-@app.route('/page6',methods=['GET','POST'])
-def renderPage3():
+@app.route('/page6' ,methods=['GET', 'POST'])
+def renderPage6():
     session["DanceName"]=request.form['DanceName']
     return render_template('page6.html')
 
+@app.route('/page7',methods=['GET','POST'])
+def renderPage7():
+    session["ChildName"]=request.form['ChildName']
+    return render_template('page7.html')
 
+@app.route('/page8',methods=['GET','POST'])
+def renderPage8():
+    session["President"]=request.form['President']
+    return render_template('page8.html')
 
+@app.route('/page9',methods=['GET','POST'])
+def renderPage9():
+    session["WoUstand"]=request.form['WoUstand']
+    session["WoUstandUser"]=request.form['WoUstandUser']
+    return render_template('page9.html')
 
-
+@app.route('/page10',methods=['GET','POST'])
+def renderPage10():
+    session["Title"]=request.form['Title']
+    return render_template('page10.html')
 
 
 if __name__=="__main__":
